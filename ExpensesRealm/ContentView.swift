52fb5b4934311725afac1bc7b272b972dc41f747
Atakan KÖMÜRCÜ <komurcu508@gmail.com>
2023-01-22 15:09:09 +0300

    Initial Commit

---
+++ ExpensesRealm/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ExpensesRealm
+//
+//  Created by Atakan Kömürcü on 22.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExpensesRealm/ExpensesRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesRealmApp.swift
+//  ExpensesRealm
+//
+//  Created by Atakan Kömürcü on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
